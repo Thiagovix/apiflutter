5a31d9150b12e54c95e076b158b60ed1b92ad569
Thiagovix <Thiago.pagoto58@gmail.com>
2024-05-09 16:17:57 -0300

    alterado

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
 }
 
 class UserListScreen extends StatefulWidget {
-  const UserListScreen({super.key});
+  const UserListScreen({Key? key}) : super(key: key);
 
   @override
   // ignore: library_private_types_in_public_api
@@ -37,8 +37,7 @@ class _UserListScreenState extends State<UserListScreen> {
   final TextEditingController tituloController = TextEditingController();
   final TextEditingController firstnameController = TextEditingController();
   final TextEditingController lastnameController = TextEditingController();
-  final TextEditingController emailController =
-      TextEditingController(); // Added for email
+  final TextEditingController emailController = TextEditingController();
   final TextEditingController pictureController = TextEditingController();
 
   @override
@@ -55,57 +54,75 @@ class _UserListScreenState extends State<UserListScreen> {
       ),
       body: Column(
         children: [
-          _buildUserList(),
+          Expanded(
+            child: FutureBuilder<List<User>>(
+              future: futureUsers,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  if (snapshot.hasError) {
+                    return Center(child: Text("Error: ${snapshot.error}"));
+                  }
+                  return ListView.builder(
+                    itemCount: snapshot.data?.length ?? 0,
+                    itemBuilder: (context, index) {
+                      User user = snapshot.data![index];
+                      return ListTile(
+                        leading: CircleAvatar(
+                          backgroundImage: NetworkImage(user.picture),
+                        ),
+                        title: Text('${user.firstName} ${user.lastName}'),
+                        subtitle: Text(user.email),
+                        trailing: Wrap(
+                          spacing: 12,
+                          children: <Widget>[
+                            IconButton(
+                              icon: const Icon(Icons.edit),
+                              onPressed: () => _showEditDialog(user),
+                            ),
+                            IconButton(
+                              icon: const Icon(Icons.delete),
+                              onPressed: () => _deleteUser(user.id),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  );
+                } else {
+                  return const Center(child: CircularProgressIndicator());
+                }
+              },
+            ),
+          ),
           _buildAddUserForm(),
         ],
       ),
     );
   }
 
-  Widget _buildUserList() {
-    return Expanded(
-      child: FutureBuilder<List<User>>(
-        future: futureUsers,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            if (snapshot.hasError) {
-              return Center(child: Text("Error: ${snapshot.error}"));
-            }
-            return ListView.builder(
-              itemCount: snapshot.data?.length ?? 0,
-              itemBuilder: (context, index) {
-                User user = snapshot.data![index];
-                return ListTile(
-                  leading: CircleAvatar(
-                    backgroundImage: NetworkImage(user.picture),
-                  ),
-                  title: Text('${user.firstName} ${user.lastName}'),
-                  subtitle: Text(user.email), // Changed to display email
-                  trailing: _buildEditAndDeleteButtons(user),
-                );
-              },
-            );
-          } else {
-            return const Center(child: CircularProgressIndicator());
-          }
-        },
+  Widget _buildAddUserForm() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: <Widget>[
+          TextFormField(
+            controller: firstnameController,
+            decoration: const InputDecoration(labelText: 'First Name'),
+          ),
+          TextFormField(
+            controller: lastnameController,
+            decoration: const InputDecoration(labelText: 'Last Name'),
+          ),
+          TextFormField(
+            controller: emailController,
+            decoration: const InputDecoration(labelText: 'Email'),
+          ),
+          ElevatedButton(
+            onPressed: _addUser,
+            child: const Text('Add User'),
+          ),
+        ],
       ),
-    );
-  }
-
-  Widget _buildEditAndDeleteButtons(User user) {
-    return Wrap(
-      spacing: 12,
-      children: <Widget>[
-        IconButton(
-          icon: const Icon(Icons.edit),
-          onPressed: () => _showEditDialog(user),
-        ),
-        IconButton(
-          icon: const Icon(Icons.delete),
-          onPressed: () => _deleteUser(user.id),
-        ),
-      ],
     );
   }
 
@@ -113,8 +130,7 @@ class _UserListScreenState extends State<UserListScreen> {
     tituloController.text = user.title;
     firstnameController.text = user.firstName;
     lastnameController.text = user.lastName;
-    emailController.text =
-        user.email; // Assuming email cannot be updated, disable this field
+    emailController.text = user.email;
     pictureController.text = user.picture;
 
     showDialog(
@@ -126,17 +142,21 @@ class _UserListScreenState extends State<UserListScreen> {
             mainAxisSize: MainAxisSize.min,
             children: <Widget>[
               TextFormField(
-                  controller: tituloController,
-                  decoration: const InputDecoration(labelText: 'Title')),
+                controller: tituloController,
+                decoration: const InputDecoration(labelText: 'Title'),
+              ),
               TextFormField(
-                  controller: firstnameController,
-                  decoration: const InputDecoration(labelText: 'First Name')),
+                controller: firstnameController,
+                decoration: const InputDecoration(labelText: 'First Name'),
+              ),
               TextFormField(
-                  controller: lastnameController,
-                  decoration: const InputDecoration(labelText: 'Last Name')),
+                controller: lastnameController,
+                decoration: const InputDecoration(labelText: 'Last Name'),
+              ),
               TextFormField(
-                  controller: pictureController,
-                  decoration: const InputDecoration(labelText: 'Picture URL')),
+                controller: pictureController,
+                decoration: const InputDecoration(labelText: 'Picture URL'),
+              ),
             ],
           ),
         ),
@@ -154,12 +174,10 @@ class _UserListScreenState extends State<UserListScreen> {
   }
 
   void _updateUser(User user) {
-    // Inicializa um Map para armazenar apenas os campos permitidos para atualização
     Map<String, dynamic> dataToUpdate = {
       'firstName': firstnameController.text,
       'lastName': lastnameController.text,
       'picture': pictureController.text,
-      // Não inclua 'email' pois é proibido atualizar
     };
 
     if (tituloController.text.isNotEmpty &&
@@ -184,42 +202,18 @@ class _UserListScreenState extends State<UserListScreen> {
     });
   }
 
-  Widget _buildAddUserForm() {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Column(
-        children: <Widget>[
-          TextFormField(
-              controller: firstnameController,
-              decoration: const InputDecoration(labelText: 'First Name')),
-          TextFormField(
-              controller: lastnameController,
-              decoration: const InputDecoration(labelText: 'Last Name')),
-          TextFormField(
-              controller: emailController, // Added email input field
-              decoration: const InputDecoration(labelText: 'Email')),
-          ElevatedButton(
-            onPressed: _addUser,
-            child: const Text('Add User'),
-          ),
-        ],
-      ),
-    );
-  }
-
   void _addUser() {
     if (firstnameController.text.isNotEmpty &&
         lastnameController.text.isNotEmpty &&
         emailController.text.isNotEmpty) {
       userService
           .createUser(User(
-        id: '', // ID é gerado pela API, não precisa enviar
-        title: tituloController
-            .text, // Incluído, assumindo que você ainda quer enviar isso
+        id: '',
+        title: tituloController.text,
         firstName: firstnameController.text,
         lastName: lastnameController.text,
         email: emailController.text,
-        picture: pictureController.text, // Incluído, assumindo que é necessário
+        picture: pictureController.text,
       ))
           .then((newUser) {
         _showSnackbar('User added successfully!');
@@ -243,3 +237,251 @@ class _UserListScreenState extends State<UserListScreen> {
         .showSnackBar(SnackBar(content: Text(message)));
   }
 }
+
+
+
+// import 'package:flutter/material.dart';
+// import 'user.dart';
+// import 'user_service.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter User API Demo',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//         visualDensity: VisualDensity.adaptivePlatformDensity,
+//       ),
+//       home: const UserListScreen(),
+//     );
+//   }
+// }
+
+// class UserListScreen extends StatefulWidget {
+//   const UserListScreen({super.key});
+
+//   @override
+//   // ignore: library_private_types_in_public_api
+//   _UserListScreenState createState() => _UserListScreenState();
+// }
+
+// class _UserListScreenState extends State<UserListScreen> {
+//   late Future<List<User>> futureUsers;
+//   final UserService userService = UserService();
+
+//   final TextEditingController tituloController = TextEditingController();
+//   final TextEditingController firstnameController = TextEditingController();
+//   final TextEditingController lastnameController = TextEditingController();
+//   final TextEditingController emailController =
+//       TextEditingController(); // Added for email
+//   final TextEditingController pictureController = TextEditingController();
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     futureUsers = userService.getUsers();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('User List'),
+//       ),
+//       body: Column(
+//         children: [
+//           _buildUserList(),
+//           _buildAddUserForm(),
+//         ],
+//       ),
+//     );
+//   }
+
+//   Widget _buildUserList() {
+//     return Expanded(
+//       child: FutureBuilder<List<User>>(
+//         future: futureUsers,
+//         builder: (context, snapshot) {
+//           if (snapshot.connectionState == ConnectionState.done) {
+//             if (snapshot.hasError) {
+//               return Center(child: Text("Error: ${snapshot.error}"));
+//             }
+//             return ListView.builder(
+//               itemCount: snapshot.data?.length ?? 0,
+//               itemBuilder: (context, index) {
+//                 User user = snapshot.data![index];
+//                 return ListTile(
+//                   leading: CircleAvatar(
+//                     backgroundImage: NetworkImage(user.picture),
+//                   ),
+//                   title: Text('${user.firstName} ${user.lastName}'),
+//                   subtitle: Text(user.email), // Changed to display email
+//                   trailing: _buildEditAndDeleteButtons(user),
+//                 );
+//               },
+//             );
+//           } else {
+//             return const Center(child: CircularProgressIndicator());
+//           }
+//         },
+//       ),
+//     );
+//   }
+
+//   Widget _buildEditAndDeleteButtons(User user) {
+//     return Wrap(
+//       spacing: 12,
+//       children: <Widget>[
+//         IconButton(
+//           icon: const Icon(Icons.edit),
+//           onPressed: () => _showEditDialog(user),
+//         ),
+//         IconButton(
+//           icon: const Icon(Icons.delete),
+//           onPressed: () => _deleteUser(user.id),
+//         ),
+//       ],
+//     );
+//   }
+
+//   void _showEditDialog(User user) {
+//     tituloController.text = user.title;
+//     firstnameController.text = user.firstName;
+//     lastnameController.text = user.lastName;
+//     emailController.text =
+//         user.email; // Assuming email cannot be updated, disable this field
+//     pictureController.text = user.picture;
+
+//     showDialog(
+//       context: context,
+//       builder: (context) => AlertDialog(
+//         title: const Text("Edit User"),
+//         content: SingleChildScrollView(
+//           child: Column(
+//             mainAxisSize: MainAxisSize.min,
+//             children: <Widget>[
+//               TextFormField(
+//                   controller: tituloController,
+//                   decoration: const InputDecoration(labelText: 'Title')),
+//               TextFormField(
+//                   controller: firstnameController,
+//                   decoration: const InputDecoration(labelText: 'First Name')),
+//               TextFormField(
+//                   controller: lastnameController,
+//                   decoration: const InputDecoration(labelText: 'Last Name')),
+//               TextFormField(
+//                   controller: pictureController,
+//                   decoration: const InputDecoration(labelText: 'Picture URL')),
+//             ],
+//           ),
+//         ),
+//         actions: <Widget>[
+//           TextButton(
+//             child: const Text("Update"),
+//             onPressed: () {
+//               _updateUser(user);
+//               Navigator.of(context).pop();
+//             },
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+
+//   void _updateUser(User user) {
+//     // Inicializa um Map para armazenar apenas os campos permitidos para atualização
+//     Map<String, dynamic> dataToUpdate = {
+//       'firstName': firstnameController.text,
+//       'lastName': lastnameController.text,
+//       'picture': pictureController.text,
+//       // Não inclua 'email' pois é proibido atualizar
+//     };
+
+//     if (tituloController.text.isNotEmpty &&
+//         firstnameController.text.isNotEmpty &&
+//         lastnameController.text.isNotEmpty &&
+//         pictureController.text.isNotEmpty) {
+//       userService.updateUser(user.id, dataToUpdate).then((updatedUser) {
+//         _showSnackbar('User updated successfully!');
+//         _refreshUserList();
+//       }).catchError((error) {
+//         _showSnackbar('Failed to update user: $error');
+//       });
+//     }
+//   }
+
+//   void _deleteUser(String id) {
+//     userService.deleteUser(id).then((_) {
+//       _showSnackbar('User deleted successfully!');
+//       _refreshUserList();
+//     }).catchError((error) {
+//       _showSnackbar('Failed to delete user.');
+//     });
+//   }
+
+//   Widget _buildAddUserForm() {
+//     return Padding(
+//       padding: const EdgeInsets.all(8.0),
+//       child: Column(
+//         children: <Widget>[
+//           TextFormField(
+//               controller: firstnameController,
+//               decoration: const InputDecoration(labelText: 'First Name')),
+//           TextFormField(
+//               controller: lastnameController,
+//               decoration: const InputDecoration(labelText: 'Last Name')),
+//           TextFormField(
+//               controller: emailController, // Added email input field
+//               decoration: const InputDecoration(labelText: 'Email')),
+//           ElevatedButton(
+//             onPressed: _addUser,
+//             child: const Text('Add User'),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+
+//   void _addUser() {
+//     if (firstnameController.text.isNotEmpty &&
+//         lastnameController.text.isNotEmpty &&
+//         emailController.text.isNotEmpty) {
+//       userService
+//           .createUser(User(
+//         id: '', // ID é gerado pela API, não precisa enviar
+//         title: tituloController
+//             .text, // Incluído, assumindo que você ainda quer enviar isso
+//         firstName: firstnameController.text,
+//         lastName: lastnameController.text,
+//         email: emailController.text,
+//         picture: pictureController.text, // Incluído, assumindo que é necessário
+//       ))
+//           .then((newUser) {
+//         _showSnackbar('User added successfully!');
+//         _refreshUserList();
+//       }).catchError((error) {
+//         _showSnackbar('Failed to add user: $error');
+//       });
+//     } else {
+//       _showSnackbar('Please fill in all fields.');
+//     }
+//   }
+
+//   void _refreshUserList() {
+//     setState(() {
+//       futureUsers = userService.getUsers();
+//     });
+//   }
+
+//   void _showSnackbar(String message) {
+//     ScaffoldMessenger.of(context)
+//         .showSnackBar(SnackBar(content: Text(message)));
+//   }
+// }
